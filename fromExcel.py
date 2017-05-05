ad8261f0c381dc3d42d8a510a73e4a92012867c4
dekarh <dekarh3@gmail.com>
2017-05-05 23:11:58 +0400

    Начал пиисать fromExcel.py

---
+++ fromExcel.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+#
+# Загрузка базы фирм из excel
+#
+
+from mysql.connector import MySQLConnection, Error
+from datetime import datetime
+import openpyxl
+from openpyxl import Workbook
+
+from libScan import wj, p, B, chk, authorize, to_spisok, set_filter, l, read_config
+
+dbconfig = read_config(section='mysql')
+exlconfig = read_config(section='excel_input')
+
+wb = openpyxl.load_workbook(read_only=True, **exlconfig)
+sheet = wb[wb.sheetnames[0]]
+
+dbconn = MySQLConnection(**dbconfig)  # Открываем БД из конфиг-файла
+read_cursor = dbconn.cursor()
+write_cursor = dbconn.cursor()
+
+print('\n\n', datetime.strftime(datetime.now(), "%H:%M:%S"), 'Начинаем импорт из excel в БД')
+# read_cursor.execute('SELECT * FROM main WHERE inn >-1;')
+# rows = read_cursor.fetchall()
+
+
+row = []
+for i, row in enumerate(sheet.rows):
+    if i == 0:
+        continue
+    id = l(row[0].value)
+    id_fil = l(row[1].value)
+    name = row[2].value
+    full_name = row[3].value
+    if row[4].value.lower().startswith('г. астрах'):
+        address = row[4].value.upper() + ', ' + row[5].value.upper()
+    else:
+        address = 'АСТРАХАНСКАЯ ОБЛ, ' + row[4].value.upper().split(',')[1] + ', ' + row[4].value.upper().split(',')[0]\
+                  + ', '+ row[5].value.upper()
+    phones_all = []
+    mob = row[6].value
+    if str(type(mob)) == "<class 'str'>":
+        for phone in mob.split(';'):
+            phones_all.append((id, 'мобильный', phone))
+    no_mob = row[7].value
+    if str(type(no_mob)) == "<class 'str'>":
+        for phone in no_mob.split(';'):
+            phones_all.append((id, 'стационарный', phone))
+    fax = row[8].value
+    if str(type(fax)) == "<class 'str'>":
+        for phone in fax.split(';'):
+            phones_all.append((id, 'факс', phone))
+    q=0
+
+
+    rez_row = []
+    for j, cell in enumerate(row):
+        if cell == None:
+            rez_cell = ''
+        else:
+            if head[j] == 'inn':
+                if cell > 9999999999:
+                    rez_cell = '{:0>12d}'.format(cell)
+                else:
+                    rez_cell = '{:0>10d}'.format(cell)
+            elif head[j] == 'kpp':
+                rez_cell = '{:0>9d}'.format(cell)
+            elif head[j] == 'id' or head[j] == 'act_num':
+                rez_cell = str(cell)
+            elif head[j] == 'phone_1' or head[j] == 'phone_2' or head[j] == 'phone_3' or head[j] == 'phone_4' or \
+                            head[j] == 'phone_5':
+                rez_cell = '{:0>11d}'.format(cell)
+            elif head[j] == 'okpo' or head[j] == 'oktmo':
+                rez_cell = '{:0>8d}'.format(cell)
+            elif head[j] == 'ogrn':
+                rez_cell = '{:0>13d}'.format(cell)
+            else:
+                rez_cell = cell.strip().replace('\n',' ').replace('\r',' ').replace('"',"'")
+            rez_row.append(rez_cell)
+    ws.append(rez_row)
+    if i % 100 == 0:
+        print(datetime.strftime(datetime.now(), "%H:%M:%S"), 'Записано в excel:', i)
+
+wb.save(**exlconfig)
+dbconn.close()
+
+
